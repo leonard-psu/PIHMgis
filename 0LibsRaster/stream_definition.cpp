e5919a85c1d295e750773d911c3236a9b18b418f
leonard-psu <lnl3@psu.edu>
2018-09-21 08:22:46 -0400

    try/catch
    stream_definition

--- 0LibsRaster/stream_definition.cpp
+++ 0LibsRaster/stream_definition.cpp
@@ -1,50 +1,70 @@
+#include <QDebug>
 #include <stdio.h>
 
 #include "lsm.h"
 #include "gridio.h"
+#include "globals.h"
+
 
 // method = 1 => Area Threshold
 // method = 2 => strahler order Threshold
 
-int stream_definition(char *aread8File, char *orderFile, char *rivFile, int method, int threshold){
-    FILE *fp;
-    int err=0;
-    int i, j;
+int stream_definition(char *aread8File, char *orderFile, char *rivFile, int method, int threshold)
+{
+    if(print_debug_messages)
+        qDebug() << "INFO: Start stream_definition";
 
-    if(method==1)
-    {
-                  //*fp = fopen(aread8File, 'r');
-                  printf("\nBefore grid read %d\n",err);
-                  err=gridread(aread8File,(void ***)&elev,RPFLTDTYPE,&nx,&ny,&dx,&dy,bndbox,&csize,&mval,&filetype);
-                  printf("\nAfter grid read %d\n",err);
-                  if(err != 0)return(err);
+    try {
 
-                  for(i=0; i<nx; i++)
-                  {
-                          for(j=0; j<ny; j++)
-                          {
-                                  if(elev[i][j]>=threshold)
-                                          elev[i][j]=1;
-                                  else
-                                          elev[i][j]=mval;
-                          }
-                  }
+        //FILE *fp;
+        int err=0;
+        int i, j;
 
-                  printf("Writing output ...");
-                  printf("\nBefore grid write %d\n",err);
-                  err = gridwrite(rivFile,(void **)elev,RPFLTDTYPE,nx,ny,dx,dy,bndbox,csize,mval,filetype);
-                  printf("\nAfter grid write %d\n",err);
-                  if(err != 0) return(err);
-    }
+        if(method==1)
+        {
+            //*fp = fopen(aread8File, 'r');
+            printf("\nBefore grid read %d\n",err);
+            err=gridread(aread8File,(void ***)&elev,RPFLTDTYPE,&nx,&ny,&dx,&dy,bndbox,&csize,&mval,&filetype);
+            printf("\nAfter grid read %d\n",err);
 
-    else if(method==2)
-    {
-        printf("Method not yet implemented\n");
-    }
+            if(err != 0)
+                return(err);
 
-    else
-    {
-        printf("Method not found\n");
+            for(i=0; i<nx; i++)
+            {
+                for(j=0; j<ny; j++)
+                {
+                    if(elev[i][j]>=threshold)
+                        elev[i][j]=1;
+                    else
+                        elev[i][j]=mval;
+                }
+            }
+
+            printf("Writing output ...");
+            printf("\nBefore grid write %d\n",err);
+            err = gridwrite(rivFile,(void **)elev,RPFLTDTYPE,nx,ny,dx,dy,bndbox,csize,mval,filetype);
+            printf("\nAfter grid write %d\n",err);
+
+            if(err != 0)
+                return(err);
+        }
+
+        else if(method==2)
+        {
+            printf("Method not yet implemented\n");
+        }
+
+        else
+        {
+            printf("Method not found\n");
+        }
+
+    } catch (...) {
+
+        qDebug() << "Error: stream_definition is returning w/o checking";
+
+
     }
     return(0);
 }
